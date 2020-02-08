a689ef166d5d50a6f1b713a156989b2de1a67dc2
Simon Tibor <simtibi@gmail.com>
2020-02-08 20:43:11 +0100

    asd

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-importScripts('/simtibi.github.io/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/simtibi.github.io/_nuxt/87eb68dfaee298c1f4e8.js",
-    "revision": "d6cb4952a547491e54731b2f9303558b"
+    "url": "/_nuxt/683dcbf1adae38bbecd0.js",
+    "revision": "44b7d9cb4d3ec347154e292a1652c185"
   },
   {
-    "url": "/simtibi.github.io/_nuxt/9e81e2ec1dc1f1ee06a2.js",
+    "url": "/_nuxt/9e81e2ec1dc1f1ee06a2.js",
     "revision": "de0a4c4a07888958b5798a3e275b4930"
   },
   {
-    "url": "/simtibi.github.io/_nuxt/bed26a910be2e8f90789.js",
-    "revision": "1c6dbadcce7333d7f3d7db151f13f41e"
+    "url": "/_nuxt/c0e1ae9ce49306cf96ba.js",
+    "revision": "4810a08af5c59befe0178ebbf4401662"
   },
   {
-    "url": "/simtibi.github.io/_nuxt/c0e1ae9ce49306cf96ba.js",
-    "revision": "4810a08af5c59befe0178ebbf4401662"
+    "url": "/_nuxt/e40a76d941f7e82ef3e7.js",
+    "revision": "e3f8791fd80f0976fcfa0f8128961ade"
   }
 ], {
   "cacheId": "simtibi.github.io",
@@ -26,6 +26,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/simtibi.github.io/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/simtibi.github.io/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
