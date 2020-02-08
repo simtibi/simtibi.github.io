f3ff537412bb596ad25032e02a1f8a29e2947391
Simon Tibor <simtibi@gmail.com>
2020-02-08 19:07:26 +0100

    ad

---
+++ sw.js
@@ -0,0 +1,31 @@
+importScripts('/simtibi.github.io/_nuxt/workbox.4c4f5ca6.js')
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "/simtibi.github.io/_nuxt/7dafcb60779993c6dc65.js",
+    "revision": "33cb9e349c01a4ace82fbbfcd8cb2bd3"
+  },
+  {
+    "url": "/simtibi.github.io/_nuxt/87eb68dfaee298c1f4e8.js",
+    "revision": "d6cb4952a547491e54731b2f9303558b"
+  },
+  {
+    "url": "/simtibi.github.io/_nuxt/9e81e2ec1dc1f1ee06a2.js",
+    "revision": "de0a4c4a07888958b5798a3e275b4930"
+  },
+  {
+    "url": "/simtibi.github.io/_nuxt/c0e1ae9ce49306cf96ba.js",
+    "revision": "4810a08af5c59befe0178ebbf4401662"
+  }
+], {
+  "cacheId": "simtibi.github.io",
+  "directoryIndex": "/",
+  "cleanUrls": false
+})
+
+workbox.clientsClaim()
+workbox.skipWaiting()
+
+workbox.routing.registerRoute(new RegExp('/simtibi.github.io/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+
+workbox.routing.registerRoute(new RegExp('/simtibi.github.io/.*'), workbox.strategies.networkFirst({}), 'GET')
