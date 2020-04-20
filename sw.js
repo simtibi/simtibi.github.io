82f67529eeff8c047cb1bd19bef5f4d5e9c5896e
Simon Tibor <simtibi@gmail.com>
2020-04-20 12:42:46 +0200

    sad

--- sw.js
+++ sw.js
@@ -6,16 +6,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "348a5242207105c26ed7c02d1f7bfd03"
   },
   {
+    "url": "/_nuxt/48ccd9acffdc893d450f.js",
+    "revision": "d310eec4c0e75dc596fc6ed215b888e0"
+  },
+  {
     "url": "/_nuxt/58559082aa4266c5f3ad.js",
     "revision": "97b32431f8523e0e139871ffbd3e6d59"
   },
   {
     "url": "/_nuxt/67f4b497c0c105b84e9f.js",
     "revision": "aa723652ce49700d1029c49c0ae2a10a"
-  },
-  {
-    "url": "/_nuxt/e889bb92d6613aecb9e7.js",
-    "revision": "84e1d50a78dd6d3be3e305a1d970731d"
   }
 ], {
   "cacheId": "simtibi.github.io",
