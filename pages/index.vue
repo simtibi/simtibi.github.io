716df501e2a79e351f818e99810b07ebc05752f0
Simon Tibor <simtibi@gmail.com>
2020-04-20 12:18:25 +0200

    asdasd

---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+  .main-page
+    .hero
+      div.mb145
+        nameAnimated
+      .skills(:class='{"active": active}')
+        p.bold.white.fs20.mb38.lh1 FRONTEND DEVELOPER
+        p.bold.white.fs20.mb38.lh1 CROSS-PLATFORM APP DEVELOPER
+        p.bold.white.fs20.lh1 VUEJS ENTHUSIAST
+</template>
+
+<script>
+import nameAnimated from '~/components/nameAnimated'
+export default {
+  components: {
+    nameAnimated
+  },
+  data: () => ({
+    active: false
+  }),
+  mounted () {
+    if (!this.$isServer) {
+      setTimeout(() => {
+        this.active = true
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .main-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 90px;
+  }
+  .hero {
+    max-width: 1150px;
+    width: 100%;
+  }
+  .skills {
+    padding-left: 40px;
+    opacity: 0;
+    transition: opacity 700ms ease-in-out 3s;
+    &.active {
+      opacity: 1;
+    }
+  }
+  @media screen and (max-width: 800px) {
+    .main-page {
+      padding: 0 50px;
+    }
+    div.mb145 {
+      margin-bottom: 110px;
+    }
+  }
+  @media screen and (max-width: 610px) {
+    .main-page {
+      padding: 0 20px;
+    }
+  }
+  @media screen and (max-width: 420px) {
+    .skills {
+      padding-left: 0;
+    }
+    div.mb145 {
+      margin-bottom: 70px;
+    }
+  }
+</style>
--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-importScripts('/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/simtibi.github.io/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/01a860d788aeac7e9d60.js",
-    "revision": "348a5242207105c26ed7c02d1f7bfd03"
+    "url": "/simtibi.github.io/_nuxt/7dafcb60779993c6dc65.js",
+    "revision": "33cb9e349c01a4ace82fbbfcd8cb2bd3"
   },
   {
-    "url": "/_nuxt/58559082aa4266c5f3ad.js",
-    "revision": "97b32431f8523e0e139871ffbd3e6d59"
+    "url": "/simtibi.github.io/_nuxt/87eb68dfaee298c1f4e8.js",
+    "revision": "d6cb4952a547491e54731b2f9303558b"
   },
   {
-    "url": "/_nuxt/67f4b497c0c105b84e9f.js",
-    "revision": "aa723652ce49700d1029c49c0ae2a10a"
+    "url": "/simtibi.github.io/_nuxt/9e81e2ec1dc1f1ee06a2.js",
+    "revision": "de0a4c4a07888958b5798a3e275b4930"
   },
   {
-    "url": "/_nuxt/c7a2de7bb205c81224c3.js",
-    "revision": "8824050e4763abbdccc9369648e20ca4"
+    "url": "/simtibi.github.io/_nuxt/c0e1ae9ce49306cf96ba.js",
+    "revision": "4810a08af5c59befe0178ebbf4401662"
   }
 ], {
   "cacheId": "simtibi.github.io",
@@ -26,6 +26,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/simtibi.github.io/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/simtibi.github.io/.*'), workbox.strategies.networkFirst({}), 'GET')
